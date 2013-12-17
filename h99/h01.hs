00c06eb075a499c325c77bbd4cb75c276f1ad611
rlaidlaw <rlaidlaw@users.noreply.github.com>
2013-12-17 21:30:05 +0000

    Add alternative solution for h01

--- h99/h01.hs
+++ h99/h01.hs
@@ -1,7 +1,14 @@
 
---Q1. Find the last element of a list.
+-- Q1. Find the last element of a list.
 
 findLast :: [a] -> a
 findLast []     = error "empty list"
 findLast (x:[]) = x
 findLast (_:xs) = findLast xs
+
+
+-- Alternative solution.
+
+findLast' :: [a] -> a
+findLast' []   = error "empty list"
+findLast' list = head (reverse list)
