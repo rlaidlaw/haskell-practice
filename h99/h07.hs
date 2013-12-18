c82f6ea76745cf2e9e03fbba3526d7fddc358793
rlaidlaw <rlaidlaw@users.noreply.github.com>
2013-12-18 18:47:01 +0000

    Add solution for h07

---
+++ h99/h07.hs
@@ -0,0 +1,9 @@
+
+-- Q7. Flatten a nested list structure.
+
+data NestedList a = Elem a | List [NestedList a]
+
+flatten :: NestedList a -> [a]
+flatten (Elem x)      = [x]
+flatten (List (x:xs)) = flatten x ++ flatten (List xs)
+flatten (List [])     = []
