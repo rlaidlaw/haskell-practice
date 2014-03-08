e26d5e0cb252cbd0f8cda5170c3d3dad5a95d1b9
rlaidlaw <rlaidlaw@users.noreply.github.com>
2014-03-08 01:22:05 +0000

    Change function name

--- euler/e002.hs
+++ euler/e002.hs
@@ -2,6 +2,6 @@ import Data.List
 
 -- Q2. Find the sum of the even-valued terms in the Fibonacci sequence
 --     whose values do not exceed four million.
-sumFibs :: Integer
-sumFibs = foldl' (+) 0 (filter (\n -> (mod n 2) == 0) (takeWhile (< 4000000) fibs))
+euler002 :: Integer
+euler002 = foldl' (+) 0 (filter (\n -> (mod n 2) == 0) (takeWhile (< 4000000) fibs))
     where fibs = 0 : scanl (+) 1 fibs
