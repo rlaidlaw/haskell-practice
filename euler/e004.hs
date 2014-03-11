42ed67b0f95046d65e060b98889c632b935f3b4f
rlaidlaw <rlaidlaw@users.noreply.github.com>
2014-03-11 01:50:05 +0000

    Crunch it into a single function

--- euler/e004.hs
+++ euler/e004.hs
@@ -1,6 +1,4 @@
 -- Q4. Find the largest palindrome made from the product of two 3-digit numbers.
 euler004 :: Integer
-euler004 = maximum (filter (isPalindrome) [x * y | x <- [100..999], y <- [100..999]])
-
-isPalindrome :: Integer -> Bool
-isPalindrome n = (show n) == reverse (show n)
+euler004 = maximum [x * y | x <- [100..999], y <- [100..999], isPalindrome (x * y)]
+  where isPalindrome n = (show n) == reverse (show n)
