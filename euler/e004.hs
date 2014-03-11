f055d8c686ca647744553b6bea10aa7b794df413
rlaidlaw <rlaidlaw@users.noreply.github.com>
2014-03-11 01:32:11 +0000

    Update solution to use filter and maximum

--- euler/e004.hs
+++ euler/e004.hs
@@ -1,21 +1,13 @@
-import Data.List
-
 -- Q4. Find the largest palindrome made from the product of two 3-digit numbers.
---     This is a very inefficient solution that takes a long time to run!!!
 euler004 :: Integer
-euler004 = largestPalindrome (sort (productList [900..999]))
+euler004 = maximum (filter (isPalindrome) [x * y | x <- [100..999], y <- [100..999]])
 
-largestPalindrome :: [Integer] -> Integer
-largestPalindrome xs
-  | isPalindrome (show (last xs)) = last xs
-  | otherwise                     = largestPalindrome (init xs)
+isPalindrome :: Integer -> Bool
+isPalindrome n = isPalindrome' (show n)
 
-isPalindrome :: Eq a => [a] -> Bool
-isPalindrome []  = True
-isPalindrome [_] = True
-isPalindrome (x:xs)
-  | x == last xs = isPalindrome (init xs)
+isPalindrome' :: Eq a => [a] -> Bool
+isPalindrome' []  = True
+isPalindrome' [_] = True
+isPalindrome' (x:xs)
+  | x == last xs = isPalindrome' (init xs)
   | otherwise    = False
-
-productList :: [Integer] -> [Integer]
-productList xs = nub [x * y | x <- xs, y <- xs]
